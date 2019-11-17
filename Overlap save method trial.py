35670b60b967823f4ba72dc3e6d24813eabf6129
Arnab Das <dasarnab1122@gmail.com>
2019-11-17 13:00:07 +0530

    Update Overlap save method trial.py

--- Overlap save method trial.py
+++ Overlap save method trial.py
@@ -1,80 +1,83 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 16 18:39:03 2019
+Created on Sun Nov 17 12:24:13 2019
 
 @author: Arnab Das
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 xn = [1,2,3,4,5,6,7,8,9]
 hn = [1,2,3]
 
-m = 3 #length of each subdomain
-l = len(hn)
+m = len(hn)
+l = 4
 N = m+l-1
-n_final = len(xn) + l - 1
 
 if(N<l):
     print("N is smaller than l......exiting")
     exit()
     
 # creating subdomains
-xn1 = xn[0:m]
-xn2 = xn[m:2*m]
-xn3 = xn[2*m:3*m]
+xn1=[];xn2=[];xn3=[];xn4=[]
 
-xn11 = [] ; xn22 = [] ; xn33 = [] ; xn44 = []
-for i in range(N-m):
-    xn11.append(0)
-for i in range(m):
-    xn11.append(xn1[i])
-    
-for j in range(N-m,0,-1):
-    xn22.append(xn11[N-j])
-for j in range(m):
-    xn22.append(xn2[j])
-    
-for k in range(N-m,0,-1):
-    xn33.append(xn22[N-k])
-for k in range(m):
-    xn33.append(xn3[k])
-
-for a in range(N-m,0,-1):
-    xn44.append(xn33[N-a])
-for a in range(m):
-    xn44.append(0)
-    
-
-for i in range(N-l):
+#zero padding hn    
+for j in range(N-m):
     hn.append(0)
     
-temp = np.copy(hn)
-hn1 = np.copy(temp)
+    
+for i in range(m-1):
+    xn1.append(0)
+try:
+    for i in range(N-(m-1)):
+        xn1.append(xn[i])
+except:
+    for i in range(N-len(xn1)):
+        xn1.append(0)
+    
+for i in range(m-1):
+    xn2.append(xn1[N-(m-1)+i])
+try:
+    for i in range(N-(m-1)):
+        xn2.append(xn[N-(m-1)+i+0*l])
+except:
+    for i in range(N-len(xn2)):
+        xn2.append(0)
+    
+for i in range(m-1):
+    xn3.append(xn2[N-(m-1)+i])
+try:
+    for i in range(N-(m-1)):
+        xn3.append(xn[N-(m-1)+1*l+i]) 
+except:
+    for i in range(N-len(xn3)):
+        xn3.append(0)
+    
+for i in range(m-1):
+    xn4.append(xn3[N-(m-1)+i])
+for i in range(N-(m-1)):
+    xn4.append(0) 
 
 """Circular Convolution"""
-y11 = [] ; y22 = [] ; y33 = [] ; y44 = []
+y1 = [];y2=[];y3=[];y4=[]
+temp = np.copy(hn)
+hn1 = np.copy(temp)
 #to invert
 for i in range(1,N,1):
     hn1[i] = temp[N-i]
-    
-n = np.linspace(0,N,N)
 
 for m in range(N):
     temp = np.roll(hn1,m) # Circular Shift
     mult1 = mult2 = mult3 = mult4 = 0
     for n in range(N):
-       mult1 += np.dot(xn11[n],temp[n])
-       mult2 += np.dot(xn22[n],temp[n])
-       mult3 += np.dot(xn33[n],temp[n])
-       mult4 += np.dot(xn44[n],temp[n])
-    y11.append(mult1);y22.append(mult2);y33.append(mult3);y44.append(mult4)
-    
-#removing m-1 data points
-y1=[];y2=[];y3=[];y4=[]
-for i in range(N-(m-1),N,1):
-    y1.append(y11[i]);y2.append(y22[i]);y3.append(y33[i]);y4.append(y44[i])
-    
+       mult1 += np.dot(xn1[n],temp[n])
+       mult2 += np.dot(xn2[n],temp[n])
+       mult3 += np.dot(xn3[n],temp[n])
+       mult4 += np.dot(xn4[n],temp[n])
+    y1.append(mult1)
+    y2.append(mult2)
+    y3.append(mult3)
+    y4.append(mult4)
+
+y1 = y1[(m-(l-1)):]; y2 = y2[(m-(l-1)):]; y3 = y3[(m-(l-1)):]; y4 = y4[(m-(l-1)):]
 y = y1+y2+y3+y4
-      
