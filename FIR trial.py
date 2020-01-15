1ed49af94c725a3b21d73a51c2e0b942854bdd21
Arnab Das <dasarnab1122@gmail.com>
2020-01-15 22:24:41 +0530

    Add files via upload

--- FIR trial.py
+++ FIR trial.py
@@ -13,9 +13,11 @@ c3 -> mag respomse of linear phase FIR filter
 """
 
 import numpy as np
+import scipy.integrate
+import matplotlib.pyplot as plt
 
 def window(c2,N):
-    n = np.linspace(0,N,N)
+    n = np.arange(0,N,1)
     if c2 == 1:
         print("Rectangular Window")
         w = np.zeros(N)
@@ -26,7 +28,7 @@ def window(c2,N):
         w = 0.5 - 0.5*np.cos((2*np.pi*n)/N)
     elif c2 == 3:
         print("Hamming Window")
-        w = 0.54 - 0.46*np.cos((2*np.pi*n)/N)
+        w = 0.54 + 0.46*np.cos((2*np.pi*n)/(N-1))
     else:
         print("Error 404!...Wrong Window Option!")
         quit()
@@ -64,11 +66,43 @@ def mag_res(c3,N,hn):
         quit()
     return Hw       
             
-                
+def idtft(x):
+    inte = []
+    for i in range(N):
+        f = lambda W:np.exp(1j*W*i)
+        x = scipy.integrate.quad(f,1.046,3.14)
+        y = scipy.integrate.quad(f,-3.14,-1.046)
+        num = x[0] + y[0] ; num = num/6.28
+        inte.append(num)
+    return inte
 
-def filter_type(c1,wl,wr,a,N):
-    if c1 == 1:
+def filter_type(c1,a,N):
+    if c1 == 2:
+        print("Designing a HPF")
+        w = np.linspace(-np.pi,np.pi,N)
+        hdw = np.zeros(len(w))
+        ind1 = np.where(w>=-np.pi)
+        hdw[ind1] = 1
+        ind2 = np.where(w>=-np.pi/3)
+        hdw[ind2] = 0
+        ind3 = np.where(w>=np.pi/3)
+        hdw[ind3] = 1
+    elif c1 == 1:
         print("Designing a LPF")
+        w = np.linspace(0,2*np.pi,N)
+        hdw = np.zeros(len(w))
+        ind1 = np.where(w<-np.pi)
+        hdw[ind1] = 1
+    elif c1 == 3:
+        print("Designing a BPF")
+    elif c1 == 4:
+        print("Designing a BSP")
+    else:
+        print("Error 404!...Wrong Mag_res Option!")
+        quit()
+    hdn = idtft(hdw)
+    print(hdn)
+    return hdn
 
 
 """Input"""
@@ -78,6 +112,13 @@ c2 = int(input("Enter window type(1-3): "))
 c3 = int(input("Enter mag_res type(1-4): "))
 a = (N-1)/2   #centre of symmetry
 
-filter_type(c1,wl,wr,a,N)
-window(c2,N)
-mag_res(c3,N,hn)
+filter = filter_type(c1,a,N)
+print("Filter: ")
+print(filter)
+w = window(c2,N)
+print(w)
+result = filter*w
+print("Result")
+print(result)
+
+#mag_res(c3,N,hn)
