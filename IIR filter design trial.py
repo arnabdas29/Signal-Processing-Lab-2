bb36aa3e0740846cbc50101462a4499ddbdaf0d4
Arnab Das <dasarnab1122@gmail.com>
2020-02-16 21:10:17 +0530

    Update IIR filter design trial.py

--- IIR filter design trial.py
+++ IIR filter design trial.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Feb 16 19:03:03 2020
-https://docs.scipy.org/doc/scipy/reference/signal.html#filter-design
+
 @author: Arnab Das
 """
 
@@ -14,14 +14,14 @@ ws = 100
 fs=1000
 N,wn = signal.buttord(wp,ws,3,40,analog=True)#Return the order of the lowest order digital or analog Butterworth filter that loses no more than gpass dB in the passband and has at least gstop dB attenuation in the stopband.
 b,a = signal.butter(N,wn,btype = 'low',analog=True,output='ba')#Design an Nth order digital or analog Butterworth filter and return the filter coefficients in (B,A) or (Z,P,K) form.
-z,p,k = signal.butter(N,wn,btype = 'low',analog=True,output='zpk')
+za,pa,ka = signal.butter(N,wn,btype = 'low',analog=True,output='zpk')
 
 #digital transfer function
 b1,a1 = signal.bilinear(b, a, fs)#Return a digital IIR filter from an analog one using a bilinear transform.
-z2,p2,k2 = signal.bilinear_zpk(z, p, k, fs)#Return a digital IIR filter from an analog one using a bilinear transform.
+z2,p2,k2 = signal.bilinear_zpk(za, pa, ka, fs)#Return a digital IIR filter from an analog one using a bilinear transform.
 
 #Compute frequency response of analog filter.
-w, h = signal.freqs_zpk(z, p, k, worN=np.logspace(-1, 2, 1000))#h->frequency response
+w, h = signal.freqs_zpk(za, pa, ka, worN=np.logspace(0, 2, 1000))#h->frequency response
 plt.semilogx(w, 20 * np.log10(abs(h)))
 plt.xlabel('Frequency')
 plt.ylabel('Amplitude response [dB]')
@@ -29,8 +29,8 @@ plt.grid()
 plt.show()
 
 #Compute frequency response of digital filter.
-z, p, k = signal.butter(N, wn, output='zpk', fs=1000)
-w, h = signal.freqz_zpk(z, p ,k , fs=1000)
+zd, pd, kd = signal.butter(N, wn, output='zpk', fs=1000)
+w, h = signal.freqz_zpk(zd, pd ,kd , fs=1000)
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
 ax1.set_title('Digital filter frequency response')
@@ -43,4 +43,15 @@ angles = np.unwrap(np.angle(h))
 ax2.plot(w, angles, 'g')
 ax2.set_ylabel('Angle [radians]', color='g')
 plt.axis('tight')
+plt.show()
+
+#to plot the poles and zeros in z-plane
+plt.figure()
+plt.plot(np.real(za), np.imag(za), 'xb')
+plt.plot(np.real(pa), np.imag(pa), 'or')
+plt.legend(['Zeros', 'Poles'], loc=2)
+plt.title('Pole / Zero Plot')
+plt.ylabel('Real')
+plt.xlabel('Imaginary')
+plt.grid()
 plt.show()
