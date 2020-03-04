57abb88661c9321c8a39a92a42439216c0b73603
Arnab Das <dasarnab1122@gmail.com>
2020-03-04 18:09:21 +0530

    Add files via upload

--- IIR filter design trial.py
+++ IIR filter design trial.py
@@ -1,57 +1,81 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Feb 16 19:03:03 2020
+Created on Mon Feb 17 08:54:48 2020
 
-@author: Arnab Das
+@author: cb.en.u4ece18106
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-wp=40
-ws = 100
-fs=1000
-N,wn = signal.buttord(wp,ws,3,40,analog=True)#Return the order of the lowest order digital or analog Butterworth filter that loses no more than gpass dB in the passband and has at least gstop dB attenuation in the stopband.
-b,a = signal.butter(N,wn,btype = 'low',analog=True,output='ba')#Design an Nth order digital or analog Butterworth filter and return the filter coefficients in (B,A) or (Z,P,K) form.
-za,pa,ka = signal.butter(N,wn,btype = 'low',analog=True,output='zpk')
+c = int(input("Type of filter(1-4): "))
+N = int(input("Enter N: "))
+fs = int(input("Enter fs: "))
+if c==1 or c==2:
+    fc = int(input("Enter fc: "))    
+    wc = (2*np.pi*fc)/fs
+    fc1=fc2=wc1=wc2=0
+else:
+    fc1 = int(input("Enter fc1: "))
+    fc2 = int(input("Enter fc2: "))
+    wc1 = (2*np.pi*fc1)/fs
+    wc2 = (2*np.pi*fc2)/fs
+    fc=0;wc=0
 
-#digital transfer function
-b1,a1 = signal.bilinear(b, a, fs)#Return a digital IIR filter from an analog one using a bilinear transform.
-z2,p2,k2 = signal.bilinear_zpk(za, pa, ka, fs)#Return a digital IIR filter from an analog one using a bilinear transform.
 
-#Compute frequency response of analog filter.
-w, h = signal.freqs_zpk(za, pa, ka, worN=np.logspace(0, 2, 1000))#h->frequency response
-plt.semilogx(w, 20 * np.log10(abs(h)))
-plt.xlabel('Frequency')
-plt.ylabel('Amplitude response [dB]')
-plt.grid()
-plt.show()
+def iir(c,N,fs,fc=0,wc=0,fc1=0,fc2=0,wc1=0,wc2=0):
+    if c==1:
+        b,a = signal.butter(N,wc,btype = 'low',analog=True,output='ba')#Design an Nth order digital or analog Butterworth filter and return the filter coefficients in (B,A) or (Z,P,K) form.
+        za,pa,ka = signal.butter(N,wc,btype = 'low',analog=True,output='zpk')
+        num,den = signal.zpk2tf(za,pa,ka)
+    elif c==2:
+        b,a = signal.butter(N,wc,btype = 'high',analog=True,output='ba')#Design an Nth order digital or analog Butterworth filter and return the filter coefficients in (B,A) or (Z,P,K) form.
+        za,pa,ka = signal.butter(N,wc,btype = 'high',analog=True,output='zpk')
+        num,den = signal.zpk2tf(za,pa,ka)
+    elif c==3:
+        b,a = signal.butter(N,[wc1,wc2],btype = 'bandpass',analog=True,output='ba')#Design an Nth order digital or analog Butterworth filter and return the filter coefficients in (B,A) or (Z,P,K) form.
+        za,pa,ka = signal.butter(N,[wc1,wc2],btype = 'bandpass',analog=True,output='zpk')
+        num,den = signal.zpk2tf(za,pa,ka)
+    elif c==4:
+        b,a = signal.butter(N,[wc1,wc2],btype = 'bandstop',analog=True,output='ba')#Design an Nth order digital or analog Butterworth filter and return the filter coefficients in (B,A) or (Z,P,K) form.
+        za,pa,ka = signal.butter(N,[wc1,wc2],btype = 'bandstop',analog=True,output='zpk')
+        num,den = signal.zpk2tf(za,pa,ka)
+    #Compute frequency response of analog filter.
+    w, h = signal.freqs(b,a)#h->frequency response
+    plt.semilogx(w, 20 * np.log10(abs(h)))
+    plt.xlabel('Frequency')
+    plt.ylabel('Amplitude response [dB]')
+    plt.grid()
+    plt.show()
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1)
+    ax1.set_title('Analog filter frequency response')
+    ax1.plot(w, 20 * np.log10(abs(h)), 'b')
+    ax1.set_ylabel('Amplitude [dB]', color='b')
+    ax1.set_xlabel('Frequency [Hz]')
+    ax1.grid()
+    ax2 = ax1.twinx()
+    angles = np.unwrap(np.angle(h))
+    ax2.plot(w, angles, 'g')
+    ax2.set_ylabel('Angle [radians]', color='g')
+    plt.axis('tight')
+    plt.show()
+    
+    #to plot the poles and zeros in z-plane
+    plt.figure()
+    plt.plot(np.real(za), np.imag(za), 'xb')
+    plt.plot(np.real(pa), np.imag(pa), 'or')
+    plt.legend(['Zeros', 'Poles'], loc=2)
+    plt.title('Pole / Zero Plot')
+    plt.ylabel('Real')
+    plt.xlabel('Imaginary')
+    plt.grid()
+    plt.show()
+    
+    print("Num: ");print(num)
+    print("Den: ");print(den)
+    
 
-#Compute frequency response of digital filter.
-zd, pd, kd = signal.butter(N, wn/fs, output='zpk')
-w, h = signal.freqz_zpk(zd, pd ,kd , fs=1000)
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 1, 1)
-ax1.set_title('Digital filter frequency response')
-ax1.plot(w, 20 * np.log10(abs(h)), 'b')
-ax1.set_ylabel('Amplitude [dB]', color='b')
-ax1.set_xlabel('Frequency [Hz]')
-ax1.grid()
-ax2 = ax1.twinx()
-angles = np.unwrap(np.angle(h))
-ax2.plot(w, angles, 'g')
-ax2.set_ylabel('Angle [radians]', color='g')
-plt.axis('tight')
-plt.show()
-
-#to plot the poles and zeros in z-plane
-plt.figure()
-plt.plot(np.real(za), np.imag(za), 'xb')
-plt.plot(np.real(pa), np.imag(pa), 'or')
-plt.legend(['Zeros', 'Poles'], loc=2)
-plt.title('Pole / Zero Plot')
-plt.ylabel('Real')
-plt.xlabel('Imaginary')
-plt.grid()
-plt.show()
+iir(c,N,fs,fc,wc,fc1,fc2,wc1,wc2)
